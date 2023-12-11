211ddbf88bad1819bc6d1b2bec122209aa19e195
UnwiseGiraffeX86 <stefanut.geala@gmail.com>
2023-12-11 19:29:13 +0000

    Update interface.py

--- src/UI/interface.py
+++ src/UI/interface.py
@@ -1,114 +1,97 @@
-import sys
 import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
+import os
+import pygame
+from pygame.locals import *
 from OpenGL.GL import *
-from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from stl import mesh
+import ctypes
+import sys
 
 class AirTunnelInterface:
     def __init__(self, root):
         self.root = root
-        self.root.title("Air Tunnel Simulation")
+        self.init_pygame_frame()
+        self.root.update()
 
-        # Initialize OpenGL
-        glutInit()
-        self.create_widgets()
+    def init_pygame_frame(self):
+        # Embed Pygame into Tkinter
+        os.environ['SDL_WINDOWID'] = str(self.embed.winfo_id())
+        if sys.platform == "win32":
+            os.environ['SDL_VIDEODRIVER'] = 'windib'
+
+        self.screen = pygame.display.set_mode((800, 600), DOUBLEBUF | OPENGL)
+        pygame.display.set_caption("Air Tunnel Simulation")
         self.init_gl()
 
-    def create_widgets(self):
-        # Frame for OpenGL canvas
-        self.opengl_frame = ttk.Frame(self.root, width=600, height=600)
-        self.opengl_frame.pack(side="left", expand=True, fill="both")
-
-        # OpenGL Canvas
-        self.opengl_canvas = tk.Canvas(self.opengl_frame)
-        self.opengl_canvas.pack(fill="both", expand=True)
-        self.opengl_canvas.bind("<Configure>", self.on_resize)
-
-        # Control Panel for User Interaction
-        self.control_panel = ttk.Frame(self.root)
-        self.control_panel.pack(side="right", fill="tk.Y")
-
-        # Buttons, Sliders, etc., for controlling the simulation
-        # Example: Load Model Button
-        self.load_model_button = ttk.Button(self.control_panel, text="Load Model", command=self.load_model)
-        self.load_model_button.pack(pady=10)
+        # Drag and Drop Setup
+        self.root.update()
+        self.embed.drop_target_register(DND_FILES)
+        self.embed.dnd_bind('<<Drop>>', self.on_drop)
 
     def init_gl(self):
-        # Basic OpenGL initialization
-        self.context = self.opengl_canvas.tk.call(self.opengl_canvas._w, 'context')
-        glEnable(GL_DEPTH_TEST)
+        glClearColor(0.0, 0.0, 0.0, 1.0)  # Set the background color
+        glEnable(GL_DEPTH_TEST)            # Enable depth testing for 3D rendering
+        glShadeModel(GL_SMOOTH)            # Enable smooth shading
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()                   # Reset the projection matrix
+        gluPerspective(45, (800 / 600), 0.1, 50.0)  # Define the perspective
+        glMatrixMode(GL_MODELVIEW)
 
-    def on_resize(self, event):
-        # Handle window resizing, adjust the viewport
-        width, height = event.width, event.height
-        glViewport(0, 0, width, height)
+    def on_drop(self, event):
+        # Handle the drop event
+        # Load the model file dropped into the frame
+        file_path = event.data
+        if file_path:
+            self.load_model(file_path)
 
-    def load_model(self):
-            # Open file dialog to choose an STL file
-            #file_path = filedialog.askopenfilename(filetypes=[("STL files", "*.stl")])
-            file_path = "C:/Users/stefa/Downloads/Super Flexi Gecko.stl"
-            if not file_path:
-                return  # No file was selected
+    def load_model(self, file_path):
+        # Load the model using numpy-stl
+        your_model = mesh.Mesh.from_file(file_path)
+        
+        # Create a display list
+        self.model_list = glGenLists(1)
+        glNewList(self.model_list, GL_COMPILE)
 
-            # Load the model using numpy-stl
-            self.model = mesh.Mesh.from_file(file_path)
-            self.model_list = self.create_model_list(self.model)
-
-    def create_model_list(self, model):
-        # Create a display list for the model
-        model_list = glGenLists(1)
-        glNewList(model_list, GL_COMPILE)
-            
         glBegin(GL_TRIANGLES)
-        for facet in model.vectors:
+        for facet in your_model.vectors:
             for vertex in facet:
                 glVertex3fv(vertex)
         glEnd()
 
         glEndList()
-        return model_list
-    
-    
+
+
     def draw(self):
-    # Clear the screen
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  # Clear the screen
+        glLoadIdentity()  # Reset the view
 
-        # Set up the camera (view matrix)
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
-        gluLookAt(
-            0.0, 0.0, 10.0,  # Camera position (x, y, z)
-            0.0, 0.0, 0.0,   # Look at point (center of the scene)
-            0.0, 1.0, 0.0    # Up vector (orientation of the camera)
-        )
+        # Camera settings, adjust as needed
+        gluLookAt(0, 0, -8, 0, 0, 0, 0, 1, 0)
 
-        # Apply transformations here (if needed)
-        # Example: rotating the model or changing its position
-        glRotatef(self.rotation_x, 1.0, 0.0, 0.0)  # Rotate around x-axis
-        glRotatef(self.rotation_y, 0.0, 1.0, 0.0)  # Rotate around y-axis
-
-        # Draw the loaded model (if available)
-        if hasattr(self, 'model_list'):
+        # Render the model
+        if self.model_list:
             glCallList(self.model_list)
 
-        # Swap buffers
-        glutSwapBuffers()
+        pygame.display.flip()  # Update the display
 
-
-    def update(self):
-        # Update the scene
-        self.draw()
-        self.root.after(100, self.update)
 
     def run(self):
-        # Main loop for the interface
-        self.update()
-        self.root.mainloop()
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            self.draw()
+            pygame.time.wait(10)  # Small delay for CPU relief
+
+    pygame.quit()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = AirTunnelInterface(root)
+    embed = tk.Frame(root, width=800, height=600)
+    embed.pack(side="left", expand=True, fill="both")
+    app = AirTunnelInterface(embed)
     app.run()
